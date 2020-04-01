a992956eadb4c396f6cbbcab2a0d950a617756aa
Kevin Li <kli@cern.ch>
2020-04-01 17:10:40 +0200

    Apply some re-styling.

--- dash_web_app/dash/dash_app.py
+++ dash_web_app/dash/dash_app.py
@@ -11,9 +11,7 @@ from flask import Flask
 url_base = '/dash_application/app1/'
 
 
-def get_covid_dataset():
-    arr = ['This is an example Plotly Dash App.']
-
+def read_covid_data() -> pd.DataFrame:
     today = (pd.to_datetime('today') - pd.to_timedelta('0 days')).strftime('%Y-%m-%d')
     link = f'https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-{today}.xls'
     print(f"\n*** Retrieving file {link}...")
@@ -27,6 +25,12 @@ def get_covid_dataset():
 
     print(df.head())
 
+    return df
+
+
+def make_table_from_df(df: pd.DataFrame):
+    arr = ['This is an example Plotly Dash App.']
+
     table_preview = dash_table.DataTable(
         id='table_covid',
         columns=[{"name": i, "id": i} for i in df.columns],
@@ -39,11 +43,46 @@ def get_covid_dataset():
     return arr
 
 
+def make_line_from_df(df: pd.DataFrame):
+    arr = ['This is an example Plotly Graph.']
+
+    plot = dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                dict(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': dict(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+    arr.append(plot)
+
+    return arr
+
+
 layout = html.Div([
     html.Div('This is dash app1'), html.Br(),
     dcc.Input(id='input_text'), html.Br(), html.Br(),
     html.Div(id="output"), html.Br(), html.Br(),
-    html.Div(id='target', className='w3-container w3-green w3-text-black w3-table', children=get_covid_dataset())
+    html.Div(id='target', className='w3-container w3-green w3-text-black w3-table',
+             children=make_table_from_df(read_covid_data()))
 ])
 
 
