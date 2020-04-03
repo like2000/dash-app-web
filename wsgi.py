d5498a1909321e67198c976f566537536dc62d41
Kevin Li <kli@cern.ch>
2020-04-03 16:47:01 +0200

    Try changing host...

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ from dash_web_app import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run()
+    app.run(host='dash-app-web.herokuapp.com')
     # app.run(host='0.0.0.0', debug=True)
