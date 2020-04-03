0687393ee577e67d01bdb7ccf28037ef600e4e17
Kevin Li <kli@cern.ch>
2020-04-03 19:50:45 +0200

    Deploy with factory method.

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from dash_web_app import create_app
 
-# if __name__ == '__main__':
-app = create_app()
-app.run()
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
 # app.run(host='0.0.0.0', debug=True)
