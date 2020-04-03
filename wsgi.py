54ebf8782ac0ad1bf8213236440b3a884aade6ce
Kevin Li <kli@cern.ch>
2020-04-03 15:00:29 +0200

    Change run for deployment...

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,5 @@ from dash_web_app import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', debug=True)
+    app.run()
+    # app.run(host='0.0.0.0', debug=True)
